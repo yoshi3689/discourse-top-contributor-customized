492c12146a5b3ceba5046afe09a19f6c36186bed
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-18 12:07:45 -0700

    test dynamically fetched categories

--- javascripts/top-contributors-sidebar/connectors/after-topic-list-body/top-contributors-sidebar.js
+++ javascripts/top-contributors-sidebar/connectors/after-topic-list-body/top-contributors-sidebar.js
@@ -33,15 +33,13 @@ export default {
           fetch('/categories.json')
           .then(res => res.json())
           .then(res => res.categories)
-          .then(data => {
-            console.log(data);
-            return data.map(category => {
+          .then(data => data.map(category => {
               return {
                 url: `c/${category.slug}/${category.id}`,
                 name: category.name
               };
             })
-          })
+          )
           .then(data => {
             console.log(data);
             this.set("categories", data);
