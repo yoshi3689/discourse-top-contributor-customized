50e4e741e6d4c518917aec4546ac8f498052aaf6
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-27 14:44:36 -0700

    change layout for a topic page

--- javascripts/discourse/api-initializers/init-top-contributors-sidebar.js
+++ javascripts/discourse/api-initializers/init-top-contributors-sidebar.js
@@ -1,37 +1,41 @@
-import { apiInitializer } from "discourse/lib/api";
+import {
+  apiInitializer
+} from "discourse/lib/api";
 
 export default apiInitializer("0.8", (api) => {
-    const connectorNameToDisable =
-      window.location.pathname.includes("/t/") ?
-      "after-topic-list-body" :
-      "above-timeline";
-    api.registerConnectorClass(connectorNameToDisable, "top-contributors-sidebar", {
-      shouldRender() {
-        return false;
-      },
-    });
-    api.onPageChange(() => {
-      const topicList = document.querySelector(".topic-list")
-      if (topicList) {
-        topicList.classList.add("with-sidebar");
-      }
-  
-      const realQuestionBtn = document.querySelector('#create-topic');
-      const container = document.querySelector('.side-nav-wrapper');
-      if (realQuestionBtn && container) {
-        const visibleQuestionBtn = container.querySelector('.question-btn');
-        visibleQuestionBtn.addEventListener('click', e => {
-          realQuestionBtn.click();
-        })
-      }
+  const connectorNameToDisable =
+    window.location.pathname.includes("/t/") ?
+    "after-topic-list-body" :
+    "above-timeline";
+  api.registerConnectorClass(connectorNameToDisable, "top-contributors-sidebar", {
+    shouldRender() {
+      return false;
+    },
+  });
+  api.onPageChange(() => {
+    const topicList = document.querySelector(".topic-list")
+    if (topicList) {
+      topicList.classList.add("with-sidebar");
+    }
 
-      document.addEventListener("load", (e) => {
-        const footerButtons = document.querySelector("#topic-footer-buttons");
-        if (footerButtons) {
-          const postContent = document.querySelector(".posts");
-          postContent.appendChild(footerButtons);
-        }
+    const realQuestionBtn = document.querySelector('#create-topic');
+    const container = document.querySelector('.side-nav-wrapper');
+    if (realQuestionBtn && container) {
+      const visibleQuestionBtn = container.querySelector('.question-btn');
+      visibleQuestionBtn.addEventListener('click', e => {
+        realQuestionBtn.click();
       })
-    });
-  
+    }
+
+    if (window.location.pathname.includes("/t/")) {
+      document.addEventListener("load", (e) => {
+        
+        const footerButtons = document.querySelector("#topic-footer-buttons");
+        const postContent = document.querySelector(".posts");
+        console.log(footerButtons, postContent)
+        postContent.appendChild(footerButtons);
+      })
+    }
+  });
+
 })
