41c38667c773fe1593542a4aa977aeb2704234dc
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-29 11:01:01 -0700

    test the dot dropdown

--- javascripts/discourse/api-initializers/init-top-contributors-sidebar.js
+++ javascripts/discourse/api-initializers/init-top-contributors-sidebar.js
@@ -1,81 +1,81 @@
-// import { withPluginApi } from "discourse/lib/plugin-api";
+import { withPluginApi } from "discourse/lib/plugin-api";
 
-// export default {
-//   setupComponent(attrs, component) {
+export default {
+  setupComponent(attrs, component) {
     
-//     const statuses = ["like", "share", "report"].map((status) => {
-//       return {
-//         name: status,
-//         value: status,
-//       };
-//     });
-//     component.set("statuses", statuses);
+    const statuses = ["like", "share", "report"].map((status) => {
+      return {
+        name: status,
+        value: status,
+      };
+    });
+    component.set("statuses", statuses);
 
-//     withPluginAPI("0.11", (api) => {
-//       api.onPageChange(() => {
-//         const connectorNameToDisable =
-//         window.location.pathname.includes("/t/") ?
-//         "after-topic-list-body" :
-//         "above-timeline";
-//       api.registerConnectorClass(connectorNameToDisable, "top-contributors-sidebar", {
-//         shouldRender() {
-//           return false;
-//         },
-//       });
+    withPluginAPI("0.11", (api) => {
+      api.onPageChange(() => {
+        const connectorNameToDisable =
+        window.location.pathname.includes("/t/") ?
+        "after-topic-list-body" :
+        "above-timeline";
+      api.registerConnectorClass(connectorNameToDisable, "top-contributors-sidebar", {
+        shouldRender() {
+          return false;
+        },
+      });
     
-//         const topicList = document.querySelector(".topic-list")
-//         if (topicList) {
-//           topicList.classList.add("with-sidebar");
-//         }
+        const topicList = document.querySelector(".topic-list")
+        if (topicList) {
+          topicList.classList.add("with-sidebar");
+        }
     
-//         const realQuestionBtn = document.querySelector('#create-topic');
-//         const container = document.querySelector('.side-nav-wrapper');
-//         if (realQuestionBtn && container) {
-//           const visibleQuestionBtn = container.querySelector('.question-btn');
-//           visibleQuestionBtn.addEventListener('click', e => {
-//             realQuestionBtn.click();
-//           })
-//         }
-//       })
-    
-//     })
-//   },
-//   actions: {
-//     takeAction(newStatus) {
-//     },
-    
-//   }
-// }
-
-import {
-  apiInitializer
-} from "discourse/lib/api";
-
-export default apiInitializer("0.8", (api) => {
-  api.onPageChange(() => {
-    const connectorNameToDisable =
-    window.location.pathname.includes("/t/") ?
-    "after-topic-list-body" :
-    "above-timeline";
-  api.registerConnectorClass(connectorNameToDisable, "top-contributors-sidebar", {
-    shouldRender() {
-      return false;
-    },
-  });
-
-    const topicList = document.querySelector(".topic-list")
-    if (topicList) {
-      topicList.classList.add("with-sidebar");
-    }
-
-    const realQuestionBtn = document.querySelector('#create-topic');
-    const container = document.querySelector('.side-nav-wrapper');
-    if (realQuestionBtn && container) {
-      const visibleQuestionBtn = container.querySelector('.question-btn');
-      visibleQuestionBtn.addEventListener('click', e => {
-        realQuestionBtn.click();
+        const realQuestionBtn = document.querySelector('#create-topic');
+        const container = document.querySelector('.side-nav-wrapper');
+        if (realQuestionBtn && container) {
+          const visibleQuestionBtn = container.querySelector('.question-btn');
+          visibleQuestionBtn.addEventListener('click', e => {
+            realQuestionBtn.click();
+          })
+        }
       })
-    }
-  });
+    
+    })
+  },
+  actions: {
+    takeAction(newStatus) {
+    },
+    
+  }
+}
 
-})
+// import {
+//   apiInitializer
+// } from "discourse/lib/api";
+
+// export default apiInitializer("0.8", (api) => {
+//   api.onPageChange(() => {
+//     const connectorNameToDisable =
+//     window.location.pathname.includes("/t/") ?
+//     "after-topic-list-body" :
+//     "above-timeline";
+//   api.registerConnectorClass(connectorNameToDisable, "top-contributors-sidebar", {
+//     shouldRender() {
+//       return false;
+//     },
+//   });
+
+//     const topicList = document.querySelector(".topic-list")
+//     if (topicList) {
+//       topicList.classList.add("with-sidebar");
+//     }
+
+//     const realQuestionBtn = document.querySelector('#create-topic');
+//     const container = document.querySelector('.side-nav-wrapper');
+//     if (realQuestionBtn && container) {
+//       const visibleQuestionBtn = container.querySelector('.question-btn');
+//       visibleQuestionBtn.addEventListener('click', e => {
+//         realQuestionBtn.click();
+//       })
+//     }
+//   });
+
+// })
