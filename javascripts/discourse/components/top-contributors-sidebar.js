92eb2ecb5c90c90987f168937d1972eda978beff
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-02 12:39:34 -0700

    hide question button on single topic pages

--- javascripts/discourse/components/top-contributors-sidebar.js
+++ javascripts/discourse/components/top-contributors-sidebar.js
@@ -1,7 +1,51 @@
 import Component from "@ember/component";
+import { inject as service } from "@ember/service";
+import { defaultHomepage } from "discourse/lib/utilities";
+import { and, bool } from "@ember/object/computed";
 import discourseComputed, { observes } from "discourse-common/utils/decorators";
 
 export default Component.extend({
+  router: service(),
   tagName: "",
-  shouldQuestionBtnAppear: !window.location.pathname.includes("/t/"),
+
+  @discourseComputed("router.currentRouteName")
+  displayForRoute(currentRouteName) {
+    const showOn = settings.show_on;
+    if (showOn === "homepage") {
+      return currentRouteName === `discovery.${defaultHomepage()}`;
+    } else if (showOn === "top_menu") {
+      return this.siteSettings.top_menu
+        .split("|")
+        .any((m) => `discovery.${m}` === currentRouteName);
+    } else {
+      return (
+        currentRouteName !== "full-page-search" &&
+        !currentRouteName.startsWith("admin.")
+      );
+    }
+  },
+  @discourseComputed("router.currentRouteName")
+  isOnSingleTopic(currentRouteName) {
+    return currentRouteName.includes("topic");
+  },
+
+  shouldDisplay: bool("displayForRoute"),
+  shouldHideQBtn: bool("isOnSingleTopic"),
+  // Setting a class on <html> from a component is not great
+  // but we need it for backwards compatibility
+  @observes("shouldDisplay")
+  displayChanged() {
+    document.documentElement.classList.toggle(
+      "display-search-banner",
+      this.shouldDisplay
+    );
+  },
+
+  didInsertElement() {
+    this.displayChanged();
+  },
+
+  didDestroyElement() {
+    document.documentElement.classList.remove("display-search-banner");
+  },
 });
