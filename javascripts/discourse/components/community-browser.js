deb9300d325a100e58306d57c7e5e200739a60ff
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-09 13:27:30 -0700

    omit the uncategorized from the community browser

--- javascripts/discourse/components/community-browser.js
+++ javascripts/discourse/components/community-browser.js
@@ -4,6 +4,7 @@ export default Component.extend({
   tagName: "",
   init() {
     this._super(...arguments);
+    // remove muted categories and the uncategorized which is the first item in the list
     this.set("categories", this.site.categories.filter(category => !category.isMuted).slice(1))
   },
 });
