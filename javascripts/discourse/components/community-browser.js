330acd6740852b7f8d23e0501b9cb4a2a162a24c
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-18 16:19:05 -0700

    switch back to previous version

--- javascripts/discourse/components/community-browser.js
+++ javascripts/discourse/components/community-browser.js
@@ -5,12 +5,11 @@ export default Component.extend({
   init() {
     this._super(...arguments);
     // remove muted categories and the uncategorized which is the first item in the list
-    this.set("categories", this.site.categories.filter(category => !category.isMuted).slice(1).map(c=>{
+    this.set("categories", this.site.categories.filter(category => !category.isMuted && !category.parentCategory).slice(1).map(c=>{
       let parentCategory = c.parentCategory ? `${c.parentCategory.slug}/` : "";
+      console.log(c);
       return {...c, category_url: `/c/${parentCategory}${c.slug}/${c.id}`};
     }))
-    // this.set("subcategories", this.site.categories.filter(category => category.parentCategory).map(c=>{
-    //   return {...c, category_url: `/c/${c.parentCategory.slug}${c.slug}/${c.id}`};
-    // }))
+    
   },
 });
