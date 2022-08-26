5c4134be257472bac42c8ad7e8a6f7306f48ab6b
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-26 13:02:28 -0700

    community browser

--- javascripts/discourse/components/community-browser.js
+++ javascripts/discourse/components/community-browser.js
@@ -5,6 +5,7 @@ export default Component.extend({
   init() {
     this._super(...arguments);
     // remove muted categories and the uncategorized which is the first item in the list
+    console.log(this.siteCategories)
     this.set("categories", this.site.categories.filter(category => !category.isMuted && !category.parentCategory).slice(1).map(c=>{
       let parentCategory = c.parentCategory ? `${c.parentCategory.slug}/` : "";
       return {...c, category_url: `/c/${parentCategory}${c.slug}/${c.id}`};
