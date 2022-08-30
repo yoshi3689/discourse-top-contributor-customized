56eadb6d1b29e866bee8a7d297d50db32e6a22e1
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-30 12:20:32 -0700

    dynamically render important links from the setting

--- javascripts/discourse/components/important-links.js
+++ javascripts/discourse/components/important-links.js
@@ -5,12 +5,13 @@ export default Component.extend({
   init() {
     this._super(...arguments);
     let importantLinks = settings.importantLinks
-    .split("|")
-    .filter(linkItem => !linkItem.includes("#"))
-    .map(linkItem => {
-      let linkSplit = linkItem.split(",").trim();
-      return {name: linkSplit[0], url: linkSplit[1]}
-    });
-    this.set("importantLinks", importantLinks);
+    console.log(importantLinks)
+    // .split("|")
+    // .filter(linkItem => !linkItem.includes("#"))
+    // .map(linkItem => {
+    //   let linkSplit = linkItem.split(",").trim();
+    //   return {name: linkSplit[0], url: linkSplit[1]}
+    // });
+    // this.set("importantLinks", importantLinks);
   },
 });
