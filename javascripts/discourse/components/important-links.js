1288beeae8d5201cd97f80fe0948f0743f134860
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-30 12:18:00 -0700

    dynamically render important links from the setting

---
+++ javascripts/discourse/components/important-links.js
@@ -0,0 +1,16 @@
+import Component from "@ember/component";
+
+export default Component.extend({
+  tagName: "",
+  init() {
+    this._super(...arguments);
+    let importantLinks = settings.importantLinks
+    .split("|")
+    .filter(linkItem => !linkItem.includes("#"))
+    .map(linkItem => {
+      let linkSplit = linkItem.split(",").trim();
+      return {name: linkSplit[0], url: linkSplit[1]}
+    });
+    this.set("importantLinks", importantLinks);
+  },
+});
